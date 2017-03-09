3e98e860872abcfc00bf7d61a9a1d4fa751bf8ef
lin.peng <lin.peng@all-union.com.cn>
2017-03-09 13:43:16 +0800

    20170309

--- pcduino/motor2.js
+++ pcduino/motor2.js
@@ -2,39 +2,39 @@
  * Created by Mars on 2017/3/6.
  */
 var pcduino = require("./pcduino");
+var RIGHT = pcduino.LOW;
+var LEFT = pcduino.HIGH;
 
 //初始化部分
-var setup = function (pin_pul, pin_dir) {
+var setup = function (pin_pul, pin_dir, direction) {
     console.log("pin-setup=" + pin_pul);
     pcduino.pinMode(pin_pul, pcduino.OUTPUT);
     pcduino.pinMode(pin_dir, pcduino.OUTPUT);
-    pcduino.digitalWrite(pin_dir, pcduino.HIGH);
+    pcduino.digitalWrite(pin_dir, direction);
 }
 
 //循环部分
-var loop = function (pin, ms) {
+var loop = function (pin) {
     pcduino.digitalWrite(pin, pcduino.HIGH);
-    pcduino.delay(ms);
     pcduino.digitalWrite(pin, pcduino.LOW);
-    pcduino.delay(ms);
 }
 
 //执行函数部分
-var execute = function execute(pin, ms, period) {
+var execute = function execute(pin, period) {
     var i = 0;
     while (i < period) {
-        loop(pin, ms);
+        loop(pin);
         ++i;
     }
 }
 
-var led = function (pin_pul, pin_dir, ms, period, callback) {
-    setup(pin_pul, pin_dir);
-    execute(pin_pul, ms, period);
+var led = function (pin_pul, pin_dir, direction, period, callback) {
+    setup(pin_pul, pin_dir, direction);
+    execute(pin_pul, period);
     callback;
 }
 
-led(13, 12, 1, 1000, function (error) {
+led(13, 12, RIGHT, 1000, function (error) {
     if (error) {
         console.log(error);
     } else {
