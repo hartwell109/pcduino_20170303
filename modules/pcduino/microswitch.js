6c0b2ebd018d7ac1b9017c1a497c66505cfea5a2
lin.peng <lin.peng@all-union.com.cn>
2017-03-16 17:21:39 +0800

    microswitch

--- modules/pcduino/microswitch.js
+++ modules/pcduino/microswitch.js
@@ -16,13 +16,13 @@ var setup = function (switch_pin) {
 var loop = function (switch_pin) {
     while (true) {
         var result = pcduino.digitalRead(switch_pin);
-        if (result == 1 && this.status == 0) {
+        if (result === 1 && this.status === 0) {
             this.status = result;
             pcduino.digitalWrite(13, pcduino.HIGH);
             //向主进程发送消息
             process.send({payload: switch_pin, status: this.status, time: (new Date()).toLocaleString()});
         }
-        if (result == 0 && this.status == 1) {
+        if (result === 0 && this.status === 1) {
             this.status = result;
             pcduino.digitalWrite(13, pcduino.LOW);
             process.send({payload: switch_pin, status: this.status, time: (new Date()).toLocaleString()});
