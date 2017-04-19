02fe2a69f0bf9e1c5be4ae2c377038a62f60812e
lin.peng <lin.peng@all-union.com.cn>
2017-04-19 15:49:46 +0800

    20170419

--- modules/serialPort/sendMessage.js
+++ modules/serialPort/sendMessage.js
@@ -2,11 +2,19 @@
  * Created by Mars on 2017/4/19.
  */
 var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyACM0', function (err) {
+var port = new SerialPort('/dev/ttyACM0', {
+    baudRate: 9600,
+    dataBits: 8,
+    stopBits: 1
+});
+port.open(function (err) {
     if (err) {
-        return console.log('Error: ', err.message);
+        return console.log('Error opening port: ', err.message);
     }
-    port.write('main screen turn on', function (err) {
+    port.write('main screen turn on');
+});
+port.on('open', function () {
+    port.write('h', function (err) {
         if (err) {
             return console.log('Error on write: ', err.message);
         }
